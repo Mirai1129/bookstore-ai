8026e6e66d4450941d9a1d9a8d75e58d0fb657bb
YuChia Ma <minecraftmiku831@gmail.com>
2025-11-05 00:11:48 +0800

    feat(schemas): build Pydantic Schemas and PyObjectId

---
+++ app/core/config.py
@@ -0,0 +1,28 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from app.core import ENV_FILE_PATH
+
+
+class Settings(BaseSettings):
+    API_V1_PREFIX: str = '/api/v1'
+
+    # Secrets
+    API_SERVER_PORT: int = Field(default=8080)
+    MONGODB_CONNECTION_STRING: str = Field(validation_alias='MONGODB_CONNECTION_URL')
+
+    # Database
+    DATABASE_NAME: str = Field(validation_alias='DATABASE_NAME')
+    COLLECTION_NAME_BOOKS: str = "books"
+    COLLECTION_NAME_CARTS: str = "carts"
+    COLLECTION_NAME_USERS: str = "users"
+    COLLECTION_NAME_ORDERS: str = "orders"
+
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE_PATH,
+        env_file_encoding='utf-8',
+        extra='ignore'
+    )
+
+
+settings = Settings()
---
+++ app/schemas/user.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field, ConfigDict
+
+from .common import PyObjectId
+
+
+class UserBase(BaseModel):
+    line_id: str = Field(..., description="LINE User ID")
+    name: str = Field(..., description="LINE User's Display Name")
+    email: Optional[str] = Field(None, description="Line User's email")
+
+    model_config = ConfigDict(
+        from_attributes=True,
+        arbitrary_types_allowed=True
+    )
+
+
+class User(UserBase):
+    """
+    Read User base model.
+    """
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    created_at: Optional[datetime] = Field(..., description="User's Creation Date")
+
+
+class UserCreate(UserBase):
+    """
+    Create User base model.
+    """
+    pass
+
+
+class UserUpdate(BaseModel):
+    """
+    Update User base model.
+    """
+    name: Optional[str] = None
+    email: Optional[str] = None
